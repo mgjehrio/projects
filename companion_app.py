18db77f1c3a861eb100ce9911a7273948edfbc27
mgjehrio <mgjehrio@buffalo.edu>
2022-05-31 15:57:32 -0400

    Update companion_app.py

--- companion_app.py
+++ companion_app.py
@@ -14,12 +14,13 @@ def st_display_pdf(pdf_file):
     with open(pdf_file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-   
+    st.write(pdf_display)
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 
 if state == "Poster":
-        st.markdown(pdf_display, unsafe_allow_html=True)
+       
         st_display_pdf("DRAFT_Matthew_Jehrio_Poster.pdf")
         
 elif state == "Paper":
